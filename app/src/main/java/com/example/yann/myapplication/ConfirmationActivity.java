2967147edfc73e22b4d5aef25b591a78c8b53a7e
Yann <yann.mahuet@orange.fr>
2015-10-05 16:25:02 +0200

    test commit

---
+++ app/src/main/java/com/example/yann/myapplication/ConfirmationActivity.java
@@ -0,0 +1,15 @@
+package com.example.yann.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yann on 05/10/2015.
+ */
+public class ConfirmationActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
